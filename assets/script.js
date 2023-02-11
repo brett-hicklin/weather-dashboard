19725d96699eb50aeaf707e4827dfb41cb011645
Brett Hicklin <hicklin.brett@gmail.com>
2023-02-10 19:42:14 -0500

    got addition API call to get latitude and longitude

--- assets/script.js
+++ assets/script.js
@@ -4,28 +4,36 @@
 
 
 
-// var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=Dover,NH,+1&APPID=c30d18cd0f8a02106652813da038e7c8'
+ var cityToCoord = 'http://api.openweathermap.org/geo/1.0/direct?q=Dover,NH,US&APPID=c30d18cd0f8a02106652813da038e7c8'
 
 
 
  var searchBtn = $('#searchBtn')
  searchBtn.on("click",runFetch)
 
- function runFetch(){
+ function runFetch() {
+   fetch(cityToCoord)
+     .then(function (response) {
+       return response.json();
+     })
 
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London,UK&units=imperial&APPID=e531facda3803f5942523ed720e4f547';
+     .then(function (CoordData) {
+       console.log(CoordData);
+       var latitude = CoordData[0].lat;
+       var longitude = CoordData[0].lon;
+       var coordToTemperature = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&APPID=e531facda3803f5942523ed720e4f547`;
+       fetch(coordToTemperature)
+         .then(function (response2) {
+           return response2.json();
+         })
 
-    var data = fetch(requestUrl)
-    
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-     
-    });
+         .then(function (temperatureData) {
+           console.log(temperatureData);
+           console.log(temperatureData.list[0].main.temp)
+         });
+     });
  }
-
+runFetch()
   /*
   create a city search box
   search button that fetches the url with the city name but changed to latitude and longitude
