157546b29d9696e4f1d35ce5f9a7277fe8f8df08
Brett Hicklin <hicklin.brett@gmail.com>
2023-02-12 17:25:33 -0500

    formatted the code some, completed the readme file, and commented on the code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # weather-dashboard
+
+## Description
+For this weeks challenge I created a weather dashboard that will display the current weather and 5 day forecast for the city entered by the user. It utilizes the openweathermap API to retrieve data from in order to get the weather. I used local storage to store the 10 most recent searches so the user can go back and click on that city to search it again. I learned more about using fetch/retrieving data and using it to provide the user with useful information. I also got more practice using local storage to store the recent searches from the user, and finally I got to utilize jquery and bootstrap to easily traverse the DOM and stylize the webpage.
+
+## Usage
+
+To access this Weather Dashboard, you can run it locally by using VScode's live server plugin. Right click on index file and select `Open with live server` 
+
+Or
+
+This website can be ran on Github Pages using the following link [Weather Dashboard](https://brett-hicklin.github.io/weather-dashboard/)
+
+When the website loads you will see the search bar that allows you to search for a city. Once you enter a valid city to be searched and click 'Search' you will then be shown the current temperature, wind speed, and humidity for said city, as well as a 5 day forecast with the temperature, wind speed, and humidity as well. You will also notice that when you search for a city, it will be added to a list beneath the search bar that will show your 10 most recent searches. If you click on one of those cities/buttons you will then be shown the current temperature as well as a 5 day forecast.
+
+example screenshots showing the layout is provided below: 
+
+![weather dashboard search](./assets/images/Weather-Dashboard-1.png)
+![weather dashboard results](./assets/images/Weather-Dashboard-2.png)
--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,43 @@
-
  var requestedCity = $("input:text").val();
  var btn = $(".btn");
  var unorderedCityList = $("#cityList");
 
  btn.on("click", inputValidation);
-
+//provides input validation that the user won't try to search an empty string
  function inputValidation(event) {
    event.preventDefault();
    var userInput = $("#search");
    var userInputValue = userInput.val();
+   //checks to see if the user input is an empty string, if so, alerts them to enter a city
    if (userInputValue === "") {
      alert("Please enter a city");
    } else {
+    //if it passes validation, it runs the runFetch function which gets the info from the API and displays it
      runFetch();
    }
  }
-
+// the function that kicks off the grabbing of the data from the open weather map API
  function runFetch() {
    var userInput = $("#search");
    var userInputValue = userInput.val() || $(this).text();
+   //converts the city the user entered to latitude and longitude to be used on the API
    var cityToCoord = `http://api.openweathermap.org/geo/1.0/direct?q=${userInputValue},US&APPID=c30d18cd0f8a02106652813da038e7c8`;
 
-   console.log(userInputValue);
-
    fetch(cityToCoord)
+   //provides further validation to make sure the city will return an ok status
      .then(function (response) {
-       console.log(response);
        if (response.status !== 200) {
          alert("No City found");
        }
        return response.json();
      })
-
+     // more validation to check if the city entered is valid
      .then(function (CoordData) {
        if (CoordData.length === 0) {
          alert("Please enter a valid city");
          userInput = userInput.val("");
+         // if the city is valid, it takes the latitude and longitude from the users' city and inputs into the open weather map API to get weather info
        } else {
-         console.log(CoordData);
          var latitude = CoordData[0].lat;
          var longitude = CoordData[0].lon;
          var coordToTemperature = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&APPID=e531facda3803f5942523ed720e4f547`;
@@ -47,7 +47,6 @@
            })
 
            .then(function (temperatureData) {
-             console.log(temperatureData);
              showTemperatureStats(temperatureData);
              show5DayForecast(temperatureData);
              showSearchHistory();
@@ -55,41 +54,43 @@
        }
      });
  }
-
+  //displays the temperature, wind speed, and humidity for the current day
  function showTemperatureStats(temperatureData) {
-  $("#infoDisplay").addClass("forecast mt-2 p-2")
+   $("#infoDisplay").addClass("forecast mt-2 p-2");
    var currentTemp = temperatureData.list[0].main.temp;
    var currentWind = temperatureData.list[0].wind.speed;
    var currentHumidity = temperatureData.list[0].main.humidity;
-
+  //displays current date at the top of the screen with the city searched
    var today = dayjs();
-  $("#cityName").text(
+   $("#cityName").text(
      `${temperatureData.city.name} ${today.format("M[/]D[/]YYYY")}`
    );
-   //look up degrees symbol
+  //displays the current weather's icon
    var currentIcon = temperatureData.list[0].weather[0].icon;
    var currentIconEl = $("#weatherIconCurrent");
    currentIconEl.attr(
      "src",
      `https://openweathermap.org/img/wn/${currentIcon}.png`
    );
+   //taking the id from the HTML and adding the text to display temp, wind speed and humidity
    $("#temp").text(`Temp: ${currentTemp}  ${String.fromCharCode(176)}F`);
    $("#wind").text(`Wind: ${currentWind} MPH`);
    $("#humidity").text(`Humidity: ${currentHumidity}%`);
  }
-
+ //displays the 5 day forecast temp, wind speed, and humidity data beneath current weather data
  function show5DayForecast(temperatureData) {
    var today = dayjs();
-   $('#forecastHeader').text("5 Day Forecast")
+   $("#forecastHeader").text("5 Day Forecast");
    for (var i = 1; i < 6; i++) {
-    $(`#forecastDay${[i]}`).addClass("forecast")
+    //displays the icons for the weather in the 5 day forecast as well as the date of the forecast listed
+     $(`#forecastDay${[i]}`).addClass("forecast");
      var icons = temperatureData.list[i].weather[0].icon;
      $(`#day${[i]}`).text(today.add(i, "day").format("M[/]D[/]YYYY"));
      $(`#weatherIconDay${[i]}`).attr(
        "src",
        `https://openweathermap.org/img/wn/${icons}.png`
      );
-
+      //displays the temp, wind speed, and humidity for the dates in the 5 day forecast
      var temp = temperatureData.list[i].main.temp;
      $(`#tempDay${[i]}`).text(`Temp: ${temp} ${String.fromCharCode(176)}F`);
      var wind = temperatureData.list[i].wind.speed;
@@ -98,7 +99,7 @@
      $(`#humidityDay${i}`).text(`Humidity: ${humidity}%`);
    }
  }
-
+ //removes duplicates from the search history if the user were to search the same city more than once
  function removeDuplicates(array) {
    var cities = [];
    array.forEach(function (city) {
@@ -108,26 +109,25 @@
    });
    return cities;
  }
-
+ //displays the list of 10 most recently searched cities
  function showSearchHistory() {
    var userInput = $("#search");
-   var userInputValue = userInput.val() 
-   console.log($(this));
+   var userInputValue = userInput.val();
    var userInputArray = JSON.parse(localStorage.getItem("recentCity"));
-
+  //creates an empty array if there are no recently searched cities
    if (userInputArray === null) {
      userInputArray = [];
    }
-
+   //disallows empty strings to be added to recent city search list and makes them all the same case to also help stop duplicates
    if (userInputValue) {
      userInputArray.push(userInputValue.toLowerCase());
      localStorage.setItem("recentCity", JSON.stringify(userInputArray));
    }
-
+   //empties the list each time a new input is added to stop the stacking of inputs, and also orders the search history start at most recently first
    unorderedCityList.empty();
    var cities = removeDuplicates(userInputArray);
    cities = cities.reverse();
-
+   //creates the buttons to search based on the city searched
    for (var i = 0; i < cities.length; i++) {
      if (i < 10) {
        var buttonEl = $("<button>");
